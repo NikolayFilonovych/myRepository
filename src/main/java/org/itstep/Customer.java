7baff7ebd3935171c1f340958bd64932ce5c5f87
NikolayFilonovych <64794800+NikolayFilonovych@users.noreply.github.com>
2020-05-17 23:56:31 +0300

    I have created necessary classes.

--- src/main/java/org/itstep/Customer.java
+++ src/main/java/org/itstep/Customer.java
@@ -2,5 +2,112 @@ package org.itstep;
 
 public class Customer
 {
+    private String firstName;
+    private String lastName;
+    private String patronym;
 
+    private String email;
+    private String phone;
+
+    private int age;
+    private String gender;
+
+
+    public void setFirstName(String fn)
+    {
+        this.firstName = fn;
+    }
+
+    public String getFirstName()
+    {
+        return this.firstName;
+    }
+
+
+    public void setLastName(String ln)
+    {
+        this.lastName = ln;
+    }
+
+    public String getLastName()
+    {
+        return this.lastName;
+    }
+
+
+    public void setPatronym(String p)
+    {
+        this.patronym = p;
+    }
+
+    public String getPatronym()
+    {
+        return this.patronym;
+    }
+
+
+    public void setEmail(String em)
+    {
+        this.email = em;
+    }
+
+    public String getEmail()
+    {
+        return this.email;
+    }
+
+
+    public void setPhone(String ph)
+    {
+        this.phone = ph;
+    }
+
+    public String getPhone()
+    {
+        return this.phone;
+    }
+
+
+    public void setAge(int a)
+    {
+        this.age = a;
+    }
+
+    public int getAge()
+    {
+        return this.age;
+    }
+
+
+    public void setGender(String gn)
+    {
+        this.gender = gn;
+    }
+
+    public String getGender()
+    {
+        return this.gender;
+    }
+
+    public Customer()
+    {
+        this.firstName = "fn";
+        this.lastName = "ln";
+        this.patronym = "p";
+        this.email = "adress@email.dom";
+        this.phone = "+0123456789";
+        this.age = 18;
+        this.gender = "man";
+    }
+
+    public Customer(String f, String n, String p, String e, String t, int a, String g)
+    {
+        this.firstName = f;
+        this.lastName = n;
+        this.patronym = p;
+        this.email = e;
+        this.phone = t;
+        this.age = a;
+        this.gender = g;
+    }
 }
--- src/main/java/org/itstep/Product.java
+++ src/main/java/org/itstep/Product.java
@@ -1,6 +1,6 @@
 package org.itstep;
 
-public class Product
+public class Product  //
 {
     private String name;
     private double price;
@@ -24,5 +24,50 @@ public class Product
 
         this.name = name;
         this.price = price;
+    }
+
+    public void setName(String n)
+    {
+        this.name = n;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void setSuperCategory(String spc)
+    {
+        this.superCategory = spc;
+    }
+
+    public String getSuperCategory()
+    {
+        return this.superCategory;
+    }
+
+    public void setSubCategory(String sbc)
+    {
+        this.name = sbc;
+    }
+
+    public String getSubCategory()
+    {
+        return this.name;
+    }
+
+    public void setPrice(double p)
+    {
+        this.price = p;
+    }
+
+    public double getPrice()
+    {
+        return this.price;
+    }
+
+    public String toString()
+    {
+        return " Product " + this.name + " costs " + this.price + ", belongs to " + this.superCategory + " and is " + this.subCategory;
     }
 }
